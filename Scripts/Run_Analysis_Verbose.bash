0f19c54485b4f706ec28146b2475155edde7b240
Geoff Dilly <geoffdilly@utexas.edu>
2025-06-26 17:43:04 -0500

    Alter scripts to use here() and simplify install script

--- Scripts/Run_Analysis_Verbose.bash
+++ Scripts/Run_Analysis_Verbose.bash
@@ -7,10 +7,10 @@ cd "$PROJECT_HOME"
 
 R --quiet --no-restore --file="Scripts/Load_10x_Data.R";
 #!R --quiet --no-restore --file="Scripts/Run_SoupX.R";
-R --quiet --no-restore --file="Scripts/Run_DoubletFinder.R";
-R --quiet --no-restore --file="Scripts/Merge_and_Normalize.R";
-R --quiet --no-restore --file="Scripts/Cluster_and_ID_Cells.R"; 
-R --quiet --no-restore --file="Scripts/Identify_Marker_Genes.R";
+#!R --quiet --no-restore --file="Scripts/Run_DoubletFinder.R";
+#!R --quiet --no-restore --file="Scripts/Merge_and_Normalize.R";
+#!R --quiet --no-restore --file="Scripts/Cluster_and_ID_Cells.R"; 
+#!R --quiet --no-restore --file="Scripts/Identify_Marker_Genes.R";
 #!R --quiet --no-restore --file="Scripts/Rename_Clusters.R";
-R --quiet --no-restore --file="Scripts/Make_Plots.R";
+#!R --quiet --no-restore --file="Scripts/Make_Plots.R";
 #!R --quiet --no-restore --file="Scripts/One_Var_DGE_Analysis.R";
