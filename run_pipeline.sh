1e525444426fc162f23828d7b68a9b1885140d04
Geoff Dilly <geoffdilly@utexas.edu>
2025-07-16 02:30:48 -0500

    Removed run_soupx

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -8,11 +8,10 @@ cd "$PROJECT_HOME"
 # Completed or optional scripts can commented out
 
 R --quiet --no-restore --file="R/01_load_data.R";
-#R --quiet --no-restore --file="R/01b_soupx.R";
 R --quiet --no-restore --file="R/02_doubletfinder.R";
 R --quiet --no-restore --file="R/03_normalize_and_integrate.R";
 R --quiet --no-restore --file="R/04_cluster_cells.R"; 
 R --quiet --no-restore --file="R/05_id_marker_genes.R";
 #R --quiet --no-restore --file="R/05b_rename_clusters.R";
 R --quiet --no-restore --file="R/06_make_plots.R";
-#R --quiet --no-restore --file="R/07_dge_1var.R";
+R --quiet --no-restore --file="R/07_dge_1var.R";
